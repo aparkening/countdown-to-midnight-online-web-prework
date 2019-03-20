40e63f655fb5631cb8efc6dd195511100715e15d
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 16:37:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+# 
 
-def countdown
+# Output countdown seconds while integer input is true
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+# Output countdown at the rate of actual seconds
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
